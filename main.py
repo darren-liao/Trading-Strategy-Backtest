d0a81b2a7213a13b1c24701c1b158ef78e4f0299
Darren <thedliao@gmail.com>
2024-07-08 01:12:55 -0500

    Updated golden cross strategy to handle order status notifications

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd 
-import backtrader as bt 
+import backtrader as bt  
+
 from strategies.golden_cross import golden_cross  
-from strategies.buy_hold import buy_hold
+from strategies.buy_hold import buy_hold 
+from strategies.sma import SMAStrategy
 
 cerebro = bt.Cerebro() 
 
@@ -15,8 +17,8 @@ cerebro.adddata(feed)
 
 #pick the strategy you want to test
 
-#cerebro.addstrategy(golden_cross)
-cerebro.addstrategy(buy_hold)
+cerebro.addstrategy(golden_cross)
+#cerebro.addstrategy(buy_hold) 
 
 cerebro.run() 
 cerebro.plot()
--- strategies/golden_cross.py
+++ strategies/golden_cross.py
@@ -23,19 +23,34 @@ class golden_cross(bt.Strategy):
         self.crossover = bt.indicators.CrossOver(
             self.fast_moving_average, 
             self.slow_moving_average
-        ) 
+        )  
 
-    def next(self): 
-        if self.position.size == 0: #only buy if we do not already hold a position 
+        self.order = None
+
+    def notify_order(self, order): 
+        if order.status in [order.Submitted, order.Accepted]: 
+            return 
+        
+        elif order.status in [order.Completed]:  
+            if order.isbuy(): 
+                print("Buy {} shares of {} at {}".format(self.size, self.params.ticker, order.executed.price))
+            elif order.issell(): 
+                print("Sell {} shares of {} at {}".format(self.size, self.params.ticker,order.executed.price))
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:  
+            print("Order failed") 
+
+        self.order = None     
+
+    def next(self):  
+        if self.order: 
+            return  
+        
+        elif self.position.size == 0: #only buy if we do not already hold a position 
             if self.crossover > 0: #Golden Cross - 50 day SMA crosses above 200 day SMA 
-
                 self.size = math.floor(self.broker.get_cash() / self.data.close) 
-
-                print("Buy {} shares of {} at {}".format(self.size, self.params.ticker, self.data.close[0]))
                 self.buy(size = self.size) 
 
         elif self.position.size > 0: 
-            if self.crossover < 0: #Death Cross - 50 day SMA crosses below 200 day SMA
-                print("Sell {} shares of {} at {}".format(self.size, self.params.ticker, self.data.close[0]))
-
-                self.close()
+            if self.crossover < 0: #Death Cross - 50 day SMA crosses below 200 day SMA 
+                self.close() 
